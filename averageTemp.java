18cef250669b253f1f0e7e49739f0a6abd3d374c
birthwright1 <birthwright1@gmail.com>
2022-09-22 16:29:57 -0400

    version 1.0

---
+++ averageTemp.java
@@ -0,0 +1,21 @@
+package teamp; // I know this was a type o but was lazy to go back and change teamp to temp. 
+               // it's only for testing purpose. 
+
+import java.util.*;
+
+public class averageTemp {
+
+	public static void main(String[] args) {
+		
+		int ny=60; // new York average temp
+		int lax=70; // LA average temp
+		int chi=50; // Chicago average temp 
+		int atl=80; // Atlanta average temp
+		
+		System.out.println("The average temp in N.Y. is --> " +ny);
+		System.out.println("The average temp in LAX is --> " +lax);
+		System.out.println("The average temp in Chicago is --> " +chi);
+		System.out.println("The average temp in Atlanta is --> " +atl);
+	}
+
+}
