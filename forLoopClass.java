bf9db6ba5384cb49c8dbc572467afdd2e01265d2
birthwright1 <birthwright1@gmail.com>
2022-10-06 16:54:42 -0400

    Version 1

---
+++ forLoopClass.java
@@ -0,0 +1,13 @@
+package forLoopProject;
+
+public class forLoopClass {
+
+	public static void main(String[] args) {
+		// for loop example
+		// for loop example listed below
+				for (int i = 0; i <= 20; i = i + 2) {
+			    	  System.out.println(i);
+				}
+		}
+
+}
