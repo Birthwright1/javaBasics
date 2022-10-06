8df2159ebf34a41356aac826feb1e134aab03f51
birthwright1 <birthwright1@gmail.com>
2022-10-06 16:57:35 -0400

    Version 1

---
+++ whileLoopClass.java
@@ -0,0 +1,14 @@
+package whileLoopProject;
+
+public class whileLoopClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // while loop example listed below
+  	  int x = 0;
+  	  while (x < 10) {
+  	  System.out.println(x);
+  	    x++;
+	}
+	}
+}
