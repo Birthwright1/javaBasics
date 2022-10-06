f85f96272147e0583a5c704b0692dd82f5a3c40a
birthwright1 <birthwright1@gmail.com>
2022-10-06 18:20:06 -0400

    10-6-22 assignment

---
+++ radiusClass.java
@@ -0,0 +1,15 @@
+package radius;
+
+public class radiusClass {
+	private static final double radius = 7.5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+	}
+
+}
